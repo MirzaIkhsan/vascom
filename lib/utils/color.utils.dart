9dc5f2c19d843e20782a4b35b51bd2e64ccb0649
MirzaIkhsan <mirzaikhsan181@gmail.com>
2023-10-21 06:05:12 +0700

    add utility as helper

---
+++ lib/utils/color.utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorUtils {
+  static const nileBlue = Color(0xFF1D334F);
+  static const paleBlue = Color(0xFF597393);
+  static const deepblue = Color(0xFF002060);
+  
+  static const carbonGrey = Color(0xFF5F5F5F);
+  static const frenchGrey = Color(0xFFBEBEBE);
+}
---
+++ lib/utils/text.utils.dart
@@ -0,0 +1,157 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:vascom/utils/color.utils.dart';
+
+abstract class TextUtils {
+  static _GilroyStyle get gilroyStyle => _GilroyStyle();
+  static _ProximaNovaStyle get proximaNovaStyle => _ProximaNovaStyle();
+}
+
+class _ProximaNovaStyle {
+  static const _fontFamily = "ProximaNova";
+
+  TextStyle get normal12PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 12,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get medium12PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 12,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w500,
+      );
+
+  TextStyle get semiBold12PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 12,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w600,
+      );
+
+  TextStyle get bold12PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 12,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w700,
+      );
+
+  TextStyle get normal14PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 14,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get medium14PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 14,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w500,
+      );
+
+  TextStyle get semiBold14PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 14,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w600,
+      );
+
+  TextStyle get bold14PaleBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 14,
+        color: ColorUtils.paleBlue,
+        fontWeight: FontWeight.w700,
+      );
+}
+
+class _GilroyStyle {
+  static const _fontFamily = "Gilroy";
+
+  TextStyle get normal16White => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get medium16White => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w500,
+      );
+
+  TextStyle get semiBold16White => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      );
+
+  TextStyle get bold16White => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: Colors.white,
+        fontWeight: FontWeight.w700,
+      );
+
+  TextStyle get normal28NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 28,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get medium28NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 28,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w500,
+      );
+
+  TextStyle get semiBold28NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 28,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w600,
+      );
+
+  TextStyle get bold28NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 28,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w700,
+      );
+
+  TextStyle get normal16NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w400,
+      );
+
+  TextStyle get medium16NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w500,
+      );
+
+  TextStyle get semiBold16NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w600,
+      );
+
+  TextStyle get bold16NileBlue => const TextStyle(
+        fontFamily: _fontFamily,
+        fontSize: 16,
+        color: ColorUtils.nileBlue,
+        fontWeight: FontWeight.w700,
+      );
+}
